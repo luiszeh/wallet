3da0620081397ecd0a25dad8cf08e00e9f621e34
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-10-28 23:50:38 -0300

    alterando titulo

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -59,7 +59,7 @@ class Login extends React.Component {
     return (
       <div className="Login-all">
         <section className="Login">
-          <h3>Faça Login</h3>
+          <h3>Fazer Login</h3>
           <GiWallet className="Icon" />
           <input
             className="Email"
