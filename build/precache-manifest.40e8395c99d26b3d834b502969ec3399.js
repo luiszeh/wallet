a0a3d7816cc394280925e9236817d6517247ba8d
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-10-28 23:54:26 -0300

    criando build para deploy

---
+++ build/precache-manifest.40e8395c99d26b3d834b502969ec3399.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7c03a5066fe873a7e7996d2165a7418",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c47e3b886286c345a67",
+    "url": "/static/css/main.150677f8.chunk.css"
+  },
+  {
+    "revision": "921cc8159a2078836425",
+    "url": "/static/js/2.09a3ae3d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.09a3ae3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c47e3b886286c345a67",
+    "url": "/static/js/main.30df1ed4.chunk.js"
+  },
+  {
+    "revision": "fe617c2cb41aece9e721",
+    "url": "/static/js/runtime-main.e8df815d.js"
+  }
+]);
